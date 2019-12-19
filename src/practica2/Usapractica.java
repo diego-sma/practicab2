83f04f904273c6937c45a9d1bd62badf8018d468
USUARIO <USUARIO@LAPTOP-5VO18K48>
2019-12-19 08:50:08 -0500

    Proyecto para Practica

---
+++ src/practica2/Usapractica.java
@@ -0,0 +1,16 @@
+
+package practica2;
+
+
+public class Usapractica {
+      public static void main(String[] args) {
+        
+        practica p=new practica();
+        p.setVisible(true);
+       
+        p.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
